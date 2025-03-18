b0fe723acc1215e8890726cb31cd9aa10b0d52c7
Aleksandr Ryzhov <lasimerunes@mail.ru>
2025-03-18 22:23:11 +0300

    some random version mid development idk

---
+++ StackVector.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
+
+/*      -------------------------------
+ *      |                             |
+ *      |   Объявления/Declarations   |
+ *      |                             |
+ *      -------------------------------
+ */
+
+template<class T>
+class Stack {
+public:
+    virtual ~Stack() = default;
+    virtual void push(const T& e) = 0;  // Добавление элемента в стек
+    virtual T pop() = 0;                // Удаление верхнего элемента
+    virtual bool isEmpty() = 0;         // Проверка стека на пустоту
+};
+
+
+
+template<class T>
+class StackVector<T>: public Stack<T> {
+public:
+    // КОНСТРУКТОРЫ
+    StackVector();
+    StackVector(const std::size_t &size);
+    // Копирование
+    StackVector(const StackVector & other);
+    StackVector & operator=(const StackVector & other);
+    // Перемещение
+    StackVector(StackVector && other);
+    StackVector & operator=(StackVector && other);
+    // Деструктор
+    ~StackVector();
+
+
+    // МЕТОДЫ
+    void push(const T& e);
+    T pop();
+    bool isEmpty();
+
+
+
+private:
+    std::size_t size_;              // На сколько элементов создан массив
+    std::size_t top_;               // Количество значащих элементов / Индекс последнего элемента
+    std::shared_ptr<T[]> data_;     // Сам массив
+
+};
+
+
+/*      -------------------------------
+ *      |                             |
+ *      |   Определения/Definitions   |
+ *      |                             |
+ *      -------------------------------
+ */
+
+// КОНСТУКТОРЫ
+template<class T>
+StackVector<T>::StackVector(): size_(0), top_(0), data_(nullptr) {}
+template<class T>
+StackVector<T>::StackVector(const std::size_t &size): size_(size), top_(0), data_(std::make_shared<T[size]>({0})) {}
+// Копирование
+template<class T>
+StackVector<T>::StackVector(const StackVector &other) {
+    // Насколько я понял, такой метод работы с массивами и shared_ptr работает только после C++17.
+    // В C++17 и раньше надо было писать кастомный деструктор.
+    size_ = other.size_;
+    top_ = other.top_;
+    // Здесь получается использовать идиому copy-and-swap.
+    auto tmp{std::make_shared<T[]>(size_)};
+    for(std::ptrdiff_t i = 0; i < top_; ++i) {
+        tmp[i] = other.data_[i];
+    }
+    data_.swap(tmp);
+}
+
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "StackVector.hpp"
+#include "QueueList.hpp"
+
+int main() {
+
+}
